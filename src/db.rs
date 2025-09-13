70ddb818b9c01ab8089b96977292cfefe591c0a2
GANESH TELORE <ganeshtelore4@gmail.com>
2025-09-13 13:21:28 +0530

    Database provider updated.

--- src/db.rs
+++ src/db.rs
@@ -6,7 +6,7 @@ static GLOBAL_POOL: OnceCell<PgPool> = OnceCell::const_new();
 pub async fn init_global_pool() -> &'static PgPool {
     GLOBAL_POOL
         .get_or_init(|| async {
-            PgPool::connect("postgresql://postgres:PZNGfiJMJtPdPAugyDzfknXxjxEXyuER@metro.proxy.rlwy.net:30116/railway")
+            PgPool::connect("postgresql://ethixion_user:vTKEAe17u5U8u9u8Dt3kx8PErjQQIzPS@dpg-d32hrradbo4c73aerj10-a.ohio-postgres.render.com/ethixion")
                 .await
                 .unwrap()
         })
