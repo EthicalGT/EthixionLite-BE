215288808fce09bbb0d4db4005a6d6735acbeedf
GANESH TELORE <ganeshtelore4@gmail.com>
2025-09-13 13:38:26 +0530

    da

--- src/db.rs
+++ src/db.rs
@@ -6,7 +6,7 @@ static GLOBAL_POOL: OnceCell<PgPool> = OnceCell::const_new();
 pub async fn init_global_pool() -> &'static PgPool {
     GLOBAL_POOL
         .get_or_init(|| async {
-            PgPool::connect("postgresql://ethixion_3yc2_user:rAZxPkldDTXckuueUQuBuxn93OnU1iot@dpg-d32i7o3uibrs739s3s70-a.oregon-postgres.render.com/ethixion_3yc2")
+            PgPool::connect("postgresql://ethixion_3yc2_user:rAZxPkldDTXckuueUQuBuxn93OnU1iot@dpg-d32i7o3uibrs739s3s70-a.oregon-postgres.render.com/ethixion_3yc2?sslmode=require")
                 .await
                 .unwrap()
         })
