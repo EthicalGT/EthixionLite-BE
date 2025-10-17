a3ea3d2fd82baad2c1d50f03c7abf7c9769bead8
GANESH TELORE <ganeshtelore4@gmail.com>
2025-10-17 16:45:27 +0530

    Update DB connection string to require SSL

--- src/db.rs
+++ src/db.rs
@@ -6,9 +6,11 @@ static GLOBAL_POOL: OnceCell<PgPool> = OnceCell::const_new();
 pub async fn init_global_pool() -> &'static PgPool {
     GLOBAL_POOL
         .get_or_init(|| async {
-            PgPool::connect("postgresql://ethixion_r827_user:DOAmm6Y6P4FExB0lr0Tq9C7S74LQktop@dpg-d3p24t7diees73cbn890-a.ohio-postgres.render.com/ethixion_r827")
-                .await
-                .unwrap()
+            PgPool::connect(
+                "postgresql://ethixion_r827_user:DOAmm6Y6P4FExB0lr0Tq9C7S74LQktop@dpg-d3p24t7diees73cbn890-a.ohio-postgres.render.com/ethixion_r827?sslmode=require"
+            )
+            .await
+            .expect("Failed to connect to DB")
         })
         .await
 }
