ac4db99e4c1d38fbfc7be4c1bc50ab14deb67070
GANESH TELORE <ganeshtelore4@gmail.com>
2025-10-17 16:41:04 +0530

    Update database connection strings in init_global_pool

--- src/db.rs
+++ src/db.rs
@@ -6,7 +6,7 @@ static GLOBAL_POOL: OnceCell<PgPool> = OnceCell::const_new();
 pub async fn init_global_pool() -> &'static PgPool {
     GLOBAL_POOL
         .get_or_init(|| async {
-            PgPool::connect("postgresql://postgres:PZNGfiJMJtPdPAugyDzfknXxjxEXyuER@metro.proxy.rlwy.net:30116/railway")
+            PgPool::connect("postgresql://ethixion_r827_user:DOAmm6Y6P4FExB0lr0Tq9C7S74LQktop@dpg-d3p24t7diees73cbn890-a.ohio-postgres.render.com/ethixion_r827")
                 .await
                 .unwrap()
         })
